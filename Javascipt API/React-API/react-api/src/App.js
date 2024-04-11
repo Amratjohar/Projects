193f2b706b87a98905e10570a537fdd49e787b8e
Amratjohar <amratjohar@gmail.com>
2024-04-11 09:45:37 +0530

    Javascript API

--- Javascipt API/React-API/react-api/src/App.js
+++ Javascipt API/React-API/react-api/src/App.js
@@ -1,24 +1,149 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+
+function refreshPage() {
+    window.location.reload(false);
+    
+  }
+
+function Cart({ items, productRemove, updateQuantity }) {
+     const totalAmount = items.reduce((acc, curr) => {
+      const price = parseFloat(curr.price) * curr.quantity; 
+      return acc + (isNaN(price) ? 0 : price);
+    }, 0);
+  
+    const Total = isNaN(totalAmount) ? "0.00" : totalAmount.toFixed(2);
+  
+    return (
+      <div className="cart">
+        <h2>Product Cart</h2>
+        {items.length === 0 ? (
+          <p>Your Products will be Displayed Here</p>
+        ) : (
+          <div>
+            <ul>
+              {items.map((item, index) => (
+                <li key={index}>
+                  <strong>{item.title}</strong> - ${item.price || 0} x{" "}
+                  <input
+                    type="number"
+                    value={item.quantity}
+                    onChange={(e) => updateQuantity(index, parseInt(e.target.value))}
+                    min={1}
+                  />
+                  <button onClick={() => productRemove(index)}>Remove</button>
+                  <div>
+                    <img src={item.image} alt={item.title} style={{ width: "50px", height: "auto", margin:"1rem" }} />
+                  </div>
+                  <p>{item.vendor}</p>
+                </li>
+              ))}
+            </ul>
+            <p id="totalPrice">Total: ${Total}</p>
+            <button id="btn">Pay</button>
+
+          </div>
+        )}
+      </div>
+    );
+  }
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [filteredItems, setFilteredItems] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [cartItems, setCartItems] = useState([]);
+  const [CartVisible, setCartVisible] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://cdn.shopify.com/s/files/1/0564/3685/0790/files/multiProduct.json"
+      )
+      .then((response) => {
+        const data = response.data;
+        setItems(data.categories);
+        setFilteredItems(data.categories);
+      });
+  }, []);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setFilteredItems(items);
+    } else {
+      const filtered = items.filter(
+        (item) => item.category_name.toLowerCase() === category.toLowerCase()
+      );
+      setFilteredItems(filtered);
+    }
+  };
+
+  const handleSearch = (event) => {
+    const query = event.target.value.toLowerCase();
+    setSearchQuery(query);
+    const filtered = items.map((item) => {
+      return {
+        ...item,
+        category_products: item.category_products.filter((product) =>
+          product.title.toLowerCase().includes(query) ||
+          product.vendor.toLowerCase().includes(query)
+        ),
+      };
+    });
+    setFilteredItems(filtered);
+  };
+
+  const addToCart = (product) => {
+    setCartItems([...cartItems, { ...product, quantity: 1 }]);
+    alert('Product Added to cart');
+  };
+
+  const productRemove = (index) => {
+    const updatedCart = [...cartItems];
+    updatedCart.splice(index, 1);
+    setCartItems(updatedCart);
+  };
+
+  const updateQuantity = (index, quantity) => {
+    const updatedCart = [...cartItems];
+    updatedCart[index].quantity = quantity;
+    setCartItems(updatedCart);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <button onClick={refreshPage}>
+         <h1>Our Products</h1>
+      </button>
+
+      <div className="buttons">
+        <button onClick={() => filterItems("Men")}>Men</button>
+        <button onClick={() => filterItems("Women")}>Women</button>
+        <button onClick={() => filterItems("Kids")}>Kids</button>
+        <input type="text" placeholder="Search.." value={searchQuery} onChange={handleSearch}/>
+        <div className="cart-icon" onClick={() => setCartVisible(!CartVisible)}>
+           <span>Cart ({cartItems.length})</span>
+        </div>
+      </div>
+
+      <div className="categories">
+        {filteredItems.map((category) =>
+          category.category_products.map((product) => (
+            <div className="product" key={product.title}>
+              <img src={product.image} alt={product.title} />
+              <h3>{product.title}</h3>
+              <p>Price: ${product.price} <strike>${product.compare_at_price}</strike></p>
+              <p>Vendor: {product.vendor}</p>
+              {product.badge_text && <span>{product.badge_text}</span>} <br/>
+              <button id="btn" onClick={() => addToCart(product)}>Add to Cart</button>
+            </div>
+          ))
+        )}
+      </div>
+      {CartVisible && <Cart items={cartItems} productRemove={productRemove} updateQuantity={updateQuantity} />}
     </div>
+
   );
 }
 
--- Javascipt API/script.js
+++ Javascipt API/script.js
@@ -1,26 +1,95 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const checkboxes = document.querySelectorAll('input[name="category"]');
+//     const productsContainer = document.getElementById("products-container");
+  
+
+//     // Fetch data from the API
+//     fetch(
+//         "https://cdn.shopify.com/s/files/1/0564/3685/0790/files/multiProduct.json"
+//     )
+//         .then((response) => response.json())
+//         .then((data) => {
+//             // Display all products first when no checkbox is checked
+//             displayProducts(data.categories);
+//             // Event listener for all the checkboxes
+//             checkboxes.forEach((checkbox) => {
+//                 checkbox.addEventListener("change", function () {
+//                     const selectedCategories = Array.from(
+//                         document.querySelectorAll('input[name="category"]:checked')
+//                     ).map((cb) => cb.value);
+//                     filterProducts(data.categories, selectedCategories);
+//                 });
+//             });
+//         })
+//         .catch((error) => console.error("Error fetching data:", error));
+//     // Function to display products
+//     function displayProducts(categories) {
+//         productsContainer.innerHTML = "";
+//         categories.forEach((category) => {
+//             category.category_products.forEach((product) => {
+//                 const productElement = document.createElement("div");
+//                 productElement.classList.add("product-card");
+//                 productElement.innerHTML = `
+//                 <img src=${product.image}>
+//                 <br/>
+//                 <h4>${product.vendor}</h4>
+//                 <br/>
+//                 <h4>${product.badge_text}</h4>
+//                 <br/>
+//                 <h4>Price Rs. ${product.price} | <span>${product.compare_at_price}</span></h4>
+//                 <br/>
+//                 <h5>${product.title}</h5>
+//                 `;
+//                 productsContainer.appendChild(productElement);
+//             });
+//         });
+//     }
+
+//     // Function to filter products based on selected categories
+//     function filterProducts(categories, selectedCategories) {
+//         if (selectedCategories.length === 0) {
+//             displayProducts(categories); //products display
+//         } else {
+//             const filteredProducts = categories.filter((category) =>
+//                 selectedCategories.includes(category.category_name)
+//             );
+//             displayProducts(filteredProducts);
+//         }
+//     }
+// })
+
 document.addEventListener("DOMContentLoaded", function () {
     const checkboxes = document.querySelectorAll('input[name="category"]');
+    const searchInput = document.getElementById("search");
     const productsContainer = document.getElementById("products-container");
+    let allProductsData; // Store all products data for reference
 
     // Fetch data from the API
-    fetch(
-        "https://cdn.shopify.com/s/files/1/0564/3685/0790/files/multiProduct.json"
-    )
+    fetch("https://cdn.shopify.com/s/files/1/0564/3685/0790/files/multiProduct.json")
         .then((response) => response.json())
         .then((data) => {
+            allProductsData = data.categories; 
+            // Store all products data
             // Display all products first when no checkbox is checked
-            displayProducts(data.categories);
+            displayProducts(allProductsData);
             // Event listener for all the checkboxes
             checkboxes.forEach((checkbox) => {
                 checkbox.addEventListener("change", function () {
-                    const selectedCategories = Array.from(
-                        document.querySelectorAll('input[name="category"]:checked')
-                    ).map((cb) => cb.value);
-                    filterProducts(data.categories, selectedCategories);
+                    filterProducts(selectedCategories(), searchInput.value);
                 });
+            });
+            // Event listener for search input
+            searchInput.addEventListener("input", function () {
+                filterProducts(selectedCategories(), this.value);
             });
         })
         .catch((error) => console.error("Error fetching data:", error));
+
+    // Function to get selected categories
+    function selectedCategories() {
+        return Array.from(document.querySelectorAll('input[name="category"]:checked')).map((cb) => cb.value);
+    }
+
     // Function to display products
     function displayProducts(categories) {
         productsContainer.innerHTML = "";
@@ -28,30 +97,66 @@ document.addEventListener("DOMContentLoaded", function () {
             category.category_products.forEach((product) => {
                 const productElement = document.createElement("div");
                 productElement.classList.add("product-card");
-                productElement.innerHTML = `
-                <img src=${product.image}>
-                <br/>
-                <h4>${product.vendor}</h4>
-                <br/>
-                <h4>${product.badge_text}</h4>
-                <br/>
-                <h4>Price Rs. ${product.price} | <span>${product.compare_at_price}</span></h4>
-                <br/>
-                <h5>${product.title}</h5>
-                `;
+                productElement.innerHTML = 
+                `<img src=${product.image}>
+                    <br/>
+                    <h4>${product.vendor}</h4>
+                    <br/>
+                    <h4>${product.badge_text}</h4>
+                    <br/>
+                    <h4>Price Rs. ${product.price} | <span>${product.compare_at_price}</span></h4>
+                    <br/>
+                    <h5>${product.title}</h5>
+                    <br/>
+                    <button id="btn" onClick='addCart('${product.title}')'>Add to Cart</button>`;
                 productsContainer.appendChild(productElement);
             });
         });
     }
-    // Function to filter products based on selected categories
-    function filterProducts(categories, selectedCategories) {
-        if (selectedCategories.length === 0) {
-            displayProducts(categories); //products display
-        } else {
-            const filteredProducts = categories.filter((category) =>
-                selectedCategories.includes(category.category_name)
-            );
-            displayProducts(filteredProducts);
-        }
+
+    // Function to filter products based on selected categories and search input
+    function filterProducts(selectedCategories, searchText) {
+        const filteredProducts = allProductsData.filter((category) => {
+            return selectedCategories.length === 0 || selectedCategories.includes(category.category_name);
+        }).map((category) => {
+            return {
+                ...category,
+                category_products: category.category_products.filter((product) => {
+                    return product.title.toLowerCase().includes(searchText.toLowerCase()) ||
+                            product.vendor.toLowerCase().includes(searchText.toLowerCase())  ||
+                           category.category_name.toLowerCase().includes(searchText.toLowerCase()) ;
+                })
+            };
+        });
+        displayProducts(filteredProducts);
     }
+
+   
+
+
+    // Function to filter products based on selected categories, search input, title, and brand name
+    // function filterProducts(selectedCategories, searchText) {
+    //     const filteredProducts = allProductsData.filter((category) => {
+    //         return selectedCategories.length === 0 || selectedCategories.includes(category.category_name);
+    //     }).map((category) => {
+    //         return {
+    //             ...category,
+    //             category_products: category.category_products.filter((product) => {
+    //                 const titleMatch = product.title.toLowerCase().includes(searchText.toLowerCase());
+    //                 const vendorMatch = product.vendor.toLowerCase().includes(searchText.toLowerCase());
+    //                 const categoryMatch = category.category_name.toLowerCase().includes(searchText.toLowerCase());
+    //                 return titleMatch || vendorMatch || categoryMatch;
+    //             })
+    //         };
+    //     });
+    //     displayProducts(filteredProducts);
+    // }
+
+    // // Event listener for all the checkboxes
+    // checkboxes.forEach((checkbox) => {
+    //     checkbox.addEventListener("change", function () {
+    //         filterProducts(selectedCategories(), searchInput.value);
+    //     });
+    // });
+
 });
